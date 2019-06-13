2976bacb731febf2a2da3a477c11147411be7c31
EricGabriel7 <ethegmc@gmail.com>
2019-06-13 11:39:55 -0400

    first commit

---
+++ src/EscapeSequencesTwo.java
@@ -0,0 +1,13 @@
+public class EscapeSequencesTwo {
+    public static void main( String[] args ){
+        /*Creating the asterisk pattern with
+        \n escape sequences */
+        System.out.println(
+                "Cost      Quantity   Total\n" +
+                "$1,000.00   4       $4,000.00\n" +
+                "$   50.00   8         $400.00\n" +
+                "______________________________\n" +
+                "TOTAL:      12      $4,400.00\n");
+    }
+
+}
